123f20dd1ec089021fec9cdc2468f2a58f4865db
majaliju <majalijunyc@gmail.com>
2022-08-07 19:34:08 -0400

    updated file structure + project guidelines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,3 +7,18 @@ class ApplicationController < Sinatra::Base
   end
 
 end
+
+
+# PROJECT CHECKLIST 
+# (1) 
+# Use Active Record to interact with a database.
+# (2)
+# Have at least two models with a one-to-many relationship.
+# (3)
+# At a minimum, set up the following API routes in Sinatra:
+# create and read actions for both models
+# full CRUD capability for one of the models
+# (4)
+# Build a separate React frontend application that interacts with the API to perform CRUD actions.
+# (5)
+# Use good OO design patterns. You should have separate classes for each of your models, and create instance and class methods as necessary.
