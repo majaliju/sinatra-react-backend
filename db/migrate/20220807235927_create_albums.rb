d18a487793641acbb22baf4743ae689e064e432b
majaliju <majalijunyc@gmail.com>
2022-08-08 19:19:16 -0400

    updated schema & migrations

--- db/migrate/20220807235927_create_albums.rb
+++
@@ -1,4 +0,0 @@
-class CreateAlbums < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/migrate/20220807235933_create_reviews.rb
+++ db/migrate/20220807235933_create_reviews.rb
@@ -1,4 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.integer :likes
+      t.integer :dislikes
+      t.string :comment
+      t.timestamp :created_at
+      t.timestamp :updated_at
+      t.integer :song_id
+    end
   end
 end
