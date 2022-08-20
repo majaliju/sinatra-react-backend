a75a9b77dead64ff3722b1e82cfeaec0256110e7
majaliju <majalijunyc@gmail.com>
2022-08-20 18:03:59 -0400

    hoping this works i am frustrated beyond words rn

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,17 @@
-# This is an _environment variable_ that is used by some of the Rake tasks to determine
-# if our application is running locally in development, in a test environment, or in production
-ENV["RACK_ENV"] ||= "development"
-
-# Require in Gems
 require "bundler/setup"
-Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require
 
-# Require in all files in 'app' directory
+configure :production, :development, :test do
+  db = URI.parse(ENV["DATABASE_URL"] || "postgres:///localhost/mydb")
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == "postgres" ? "postgresql" : db.scheme,
+    :host => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => "utf8",
+  )
+end
+
 require_all "app"
-
-require "active_record"
-
-ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || "postgres://localhost/mydb")
