3ee8c1bacd3a299801827639cd103ecf498f0d25
majaliju <majalijunyc@gmail.com>
2022-08-20 21:03:32 -0400

    updated

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ require "bundler/setup"
 Bundler.require(:default, ENV["RACK_ENV"])
 
 # ## FROM https://medium.com/@isphinxs/deploying-a-sinatra-app-to-heroku-7944b024f77c
-# set :database_file, "./database.yml"
+set :database_file, "./database.yml"
 
 # Require in all files in 'app' directory
 require_all "app"
