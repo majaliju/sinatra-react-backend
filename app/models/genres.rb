44ef88c16e62fd1aee4342167120156ac127135e
majaliju <majalijunyc@gmail.com>
2022-10-20 20:11:26 -0400

    added validation on 3 models

--- app/models/genres.rb
+++ app/models/genres.rb
@@ -2,6 +2,7 @@ class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
   has_many :reviews, through: :songs
+  validates :name, uniqueness: true
 
   accepts_nested_attributes_for :artists, :songs
   before_save :title_fixer
@@ -10,5 +11,10 @@ class Genre < ActiveRecord::Base
   def title_fixer
     ## add a case to capitalize after [&, -, /]
     self.name = self.name.split(/ |\_/).map(&:capitalize).join(" ")
+  end
+
+  def duplicate_checker
+    ## find a match for the current entry
+    ## if doesnt exist, then create and save
   end
 end
